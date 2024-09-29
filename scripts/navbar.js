7b608b8f83dc78e06aaf0271ca45c0e3e52ff61b
Tung <num12class615@gmail.com>
2024-09-29 19:06:28 +0700

    add address.html

---
+++ scripts/navbar.js
@@ -0,0 +1,6 @@
+const navbarJewelry = document.getElementById("navbar-jewelry");
+const navbarWedding = document.getElementById("navbar-wedding");
+const navbarDiamond = document.getElementById("navbar-diamond");
+const navbarGift = document.getElementById("navbar-gift");
+const navbarArticle = document.getElementById("navbar-article");
+const navbarBenefits = document.getElementById("navbar-benefits");
